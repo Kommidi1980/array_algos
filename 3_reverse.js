2615432ac8eb17a75efe981e0747119937c99984
Praneeth Reddy <praneethreddy@hotmail.com>
2020-02-19 12:41:14 -0800

    code_done

--- 3_reverse.js
+++ 3_reverse.js
@@ -1,5 +1,11 @@
 // Given var arr = [8,6,7,5,3,0,9] reverse the array and print the result
 // Predicted output: [9,0,3,5,7,6,8]
 // When you have solved this, create new values for your array and ensure it still works as expected.
+var arr = [8,6,7,5,3,0,9];
+var newarr =[];
+for (var i=0;i<arr.length;i++){ 
 
-// YOUR CODE HERE
+    newarr.push(arr[(arr.length-1)-i]);   
+}
+arr=newarr;
+console.log(arr);
